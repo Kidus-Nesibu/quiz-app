fbb9fc279cb0019991da2594ffb66650648b1025
KidusNesibu <knesibu3@gmail.com>
2025-01-25 16:59:09 +0300

    fixed fetching logic

--- src/api.js
+++ src/api.js
@@ -1,76 +1,79 @@
 import { delay } from "./utils.js";
 
-const apiurl ="https://opentdb.com/api.php?amount=10"
+const apiurl ="https://opentdb.com/api.php?amount=10";
 
-async function GetSessionToken() {
-    const response = await fetch("https://opentdb.com/api_token.php?command=request")
-    const data = await response.json()
-    if (data.response_code === 0)
+async function GetSessionToken()
+{    
+  const response = await fetch("https://opentdb.com/api_token.php?command=request")
+  const data = await response.json()
+  if (data.response_code === 0)
     {
-        return data.token;
+      return data.token;
     }
-    else
+    else 
     {
-        console.log("there is an error that occured while fetching the token")
+      console.log(`Error occured: ${data.response_code}`)
     }
 }
+
+/**checking for errors */
+const token = await GetSessionToken()
+console.log(token)
+
+
 async function ResetSessionToken(token)
 {
-    const response = await fetch("https://opentdb.com/api_token.php?command=reset&token={token}")
-    const data = await response.json()
+    const response = await fetch(`https://opentdb.com/api_token.php?command=reset&token=${token}`)  ;
+    const data = await response.json();
 
     if (data.response_code === 0)
     {
-        console.log("reseted the token successfully")
+        return true;
     }
     else
     {
-        console.log("faield reseting the token")
+        console.log(`Error occured: ${data.response_code}`);
+        return false;
     }
 }
 
-async function Category(){
-    const response = await fetch("https://opentdb.com/api_category.php")
-    const data = await response.json()
+/**checking for errors */
+const value = await ResetSessionToken(token);
+console.log(value);
 
-    console.log(data)
-}
 
-async function FetchQuestions(token, amount = 10, category = 9, difficulty = "medium", type = "multiple", retries = 3)
+async function Getcategory()
 {
-    const url = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}&token=${token}`;
-    const response = await fetch(url);
+    const response = await fetch("https://opentdb.com/api_category.php");
     const data = await response.json();
-  
-    if (data.response_code === 0) 
-    {
-        console.log(data.results)
-        return data.results; // Return the questions
-    } 
-    else if (data.response_code === 4) 
-    {
-      console.log("resete session it is exhausted")
-      await ResetSessionToken(token);
-      return [];
-    }
-    else if (data.response_code === 5 && retries > 0)
-    {
-      console.log("Too many requests Chill my nigga")
-      await delay(5000)
-      return await FetchQuestions(token, retries = -1)
+    return data;
+    
+}
 
-    }
-    else
-    {
-      console.log(data.response_code)
-      const errorMessage = 
-      {
-        1: "No results found for the given query.",
-        2: "Invalid parameter passed to the API.",
-        3: "Session token not found.",
-      }
-      [data.response_code] || "Unknown error occurred.";
-      throw new Error(`Failed to fetch questions: ${errorMessage}`);
-    }
+/**checking for errors  */
+const gener = await Getcategory();
+console.log(gener)
+
+async function FetchQuestions(token, amount = 5, type = "multiple")
+{
+  const url = `https://opentdb.com/api.php?amount=${amount}&type=${type}&token=${token}`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (data.response_code === 0)
+  {
+    return data;
   }
-export {GetSessionToken, ResetSessionToken, FetchQuestions, Category}
+  else 
+  {
+    console.log(`Error ocurring ${data.response_code}`)
+  }
+}
+
+
+/**checking for errors */
+const Questions = await FetchQuestions(token)
+console.log(Questions)
+
+
+export {GetSessionToken, ResetSessionToken, FetchQuestions, Getcategory}
